a96cde839dc5c99d4d95a956abea0245d35fba6c
Lorenzo Vannucci <lore.ucci@gmail.com>
2020-09-17 11:44:27 +0200

    Exposed plot arrangement parameters via C++

--- include/cpphots/plots.h
+++ include/cpphots/plots.h
@@ -11,6 +11,8 @@ namespace cpphots {
 /**
  * @brief Plot a time surface
  * 
+ * This functions accepts both one and two-dimensional time surfaces.
+ * 
  * @param ts the timesurface to be plotted
  */
 void plot_ts(const TimeSurfaceType& ts);
@@ -18,8 +20,14 @@ void plot_ts(const TimeSurfaceType& ts);
 /**
  * @brief Plot multiple time surfaces into a single figure
  * 
+ * This functions accepts both one and two-dimensional time surfaces.
+ * The arrangement of plots, in terms of rows and columns, can be optionally specified.
+ * If no arrangment is specified, a best-effort arrangement that tries to minimize the number of empty subslots is performed.
+ * 
  * @param mts the time surfaces to be plotted
+ * @param rows the number of rows for the arrangement
+ * @param cols the number of columns for the arrangement
  */
-void plot_multiple_ts(const std::vector<TimeSurfaceType>& mts);
+void plot_multiple_ts(const std::vector<TimeSurfaceType>& mts, unsigned int rows = 0, unsigned int cols = 0);
 
 }
