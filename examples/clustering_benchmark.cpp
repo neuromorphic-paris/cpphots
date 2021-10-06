c16d61b5951d4774c9b0c2aa87f65a9623450696
Lorenzo Vannucci <lore.ucci@gmail.com>
2021-10-06 15:11:53 +0200

    Added k-means to benchmark example

--- examples/clustering_benchmark.cpp
+++ examples/clustering_benchmark.cpp
@@ -9,7 +9,10 @@
 #include <cpphots/time_surface.h>
 #include <cpphots/layer.h>
 #include <cpphots/clustering/cosine.h>
+#include <cpphots/clustering/kmeans.h>
+#ifdef CPPHOTS_WITH_PEREGRINE
 #include <cpphots/clustering/gmm.h>
+#endif
 #include <cpphots/run.h>
 
 #include "commons.h"
@@ -58,21 +61,32 @@ int main() {
         cpphots::Layer layer(cpphots::create_pool_ptr<cpphots::LinearTimeSurface>(100, 100, 5, 5, 500., 1),
                              new cpphots::CosineClusterer(10));
         auto [tr, ex] = measure_times(layer, n_training, n_events);
-        std::cout << "  HOTS | " << std::setw(9) << std::setprecision(5) << tr << " | " << std::setw(9) << std::setprecision(5) << ex << std::endl;
+        std::cout << " cosine | " << std::setw(9) << std::setprecision(5) << tr << " | " << std::setw(9) << std::setprecision(5) << ex << std::endl;
     }
+
+    #ifdef CPPHOTS_WITH_PEREGRINE
 
     {
         cpphots::Layer layer(cpphots::create_pool_ptr<cpphots::LinearTimeSurface>(100, 100, 5, 5, 500., 1),
                              new cpphots::GMMClusterer(cpphots::GMMClusterer::S_GMM, 10, 5, 8, 0.01, 20));
         auto [tr, ex] = measure_times(layer, n_training, n_events);
-        std::cout << " S-GMM | " << std::setw(9) << std::setprecision(5) << tr << " | " << std::setw(9) << std::setprecision(5) << ex << std::endl;
+        std::cout << "  S-GMM | " << std::setw(9) << std::setprecision(5) << tr << " | " << std::setw(9) << std::setprecision(5) << ex << std::endl;
     }
 
     {
         cpphots::Layer layer(cpphots::create_pool_ptr<cpphots::LinearTimeSurface>(100, 100, 5, 5, 500., 1),
                              new cpphots::GMMClusterer(cpphots::GMMClusterer::U_S_GMM, 10, 5, 8, 0.01, 20));
         auto [tr, ex] = measure_times(layer, n_training, n_events);
-        std::cout << "uS-GMM | " << std::setw(9) << std::setprecision(5) << tr << " | " << std::setw(9) << std::setprecision(5) << ex << std::endl;
+        std::cout << " uS-GMM | " << std::setw(9) << std::setprecision(5) << tr << " | " << std::setw(9) << std::setprecision(5) << ex << std::endl;
+    }
+
+    #endif
+
+    {
+        cpphots::Layer layer(cpphots::create_pool_ptr<cpphots::LinearTimeSurface>(100, 100, 5, 5, 500., 1),
+                             new cpphots::KMeansClusterer(10, 20));
+        auto [tr, ex] = measure_times(layer, n_training, n_events);
+        std::cout << "k-means | " << std::setw(9) << std::setprecision(5) << tr << " | " << std::setw(9) << std::setprecision(5) << ex << std::endl;
     }
 
     return 0;
